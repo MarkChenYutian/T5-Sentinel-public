4b2c794ac0d8fd76b5f6209b35aad9f0ef9a4d0d
Yutian Chen <markchenyutian@gmail.com>
2023-10-19 02:04:41 -0400

    Add checkpoint auto download feature

--- data/download.py
+++ data/download.py
@@ -17,7 +17,15 @@ def downloadAndExtractTo(url: str, to: Path):
     file = zipfile.ZipFile(io.BytesIO(requests.get(url, stream=True).content))
     file.extractall(to)
 
-
+def downloadAndSaveTo(url, filename):
+    print(f"Downloading: {url} => {filename}")
+    with requests.get(url, stream=True) as r:
+        if r.status_code == 200:
+            r.raise_for_status()
+            with open(filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192): f.write(chunk)
+        else:
+            print(f"Failed to download file, status code: {r.status_code}")
 
 if __name__ == "__main__":
     from_files = [Path(source, from_subset) 
@@ -27,6 +35,12 @@ if __name__ == "__main__":
                   for to_subset in to_subsets
                   for source in sources]
     
+    print("Download Checkpoints (models)")
+    downloadAndSaveTo("https://github.com/MarkChenYutian/T5-Sentinel-public/releases/download/InitialCommit/solaiman-detector-base.pt", Path("data", "checkpoint", "solaiman-detector-base.pt"))
+    downloadAndSaveTo("https://github.com/MarkChenYutian/T5-Sentinel-public/releases/download/InitialCommit/T5Hidden.0622.pt", Path("data", "checkpoint", "T5Hidden.0622.pt"))
+    downloadAndSaveTo("https://github.com/MarkChenYutian/T5-Sentinel-public/releases/download/InitialCommit/T5Sentinel.0613.pt", Path("data", "checkpoint", "T5Sentinel.0613.pt"))
+    
+    print("Download Dataset")
     downloadAndExtractTo("https://zenodo.org/records/8285326/files/GPT2.zip?download=1", Path("data", "split", "gpt2-output"))
     downloadAndExtractTo("https://zenodo.org/records/8285326/files/ChatGPT.zip?download=1", Path("data", "split", "open-gpt-text"))
     downloadAndExtractTo("https://zenodo.org/records/8285326/files/LLaMA.zip?download=1", Path("data", "split", "open-llama-text"))
