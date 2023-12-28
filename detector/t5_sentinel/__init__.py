d049953b67c4aa8aae2bf2a5d908878a29312395
Hao-Kang <haok1402@gmail.com>
2023-12-28 10:45:36 -0500

    fix path not resolving correctly by removing the extra `models` subpath

--- detector/t5_sentinel/__init__.py
+++ detector/t5_sentinel/__init__.py
@@ -2,5 +2,5 @@ import yaml
 from detector.t5_sentinel.types import Config
 
 
-with open('detector/models/arbitrary/settings.yaml', 'r') as f:
+with open("detector/t5_sentinel/settings.yaml", "r") as f:
     config = Config(**yaml.safe_load(f))
--- detector/t5_sentinel/__main__.py
+++ detector/t5_sentinel/__main__.py
@@ -16,20 +16,20 @@ from detector.t5_sentinel.__init__ import config
 # Dataset and Dataloader
 ##############################################################################
 train_loader = DataLoader(
-    train_dataset := Dataset('train-dirty'),
-    collate_fn=train_dataset.collate_fn, 
+    train_dataset := Dataset("train-dirty"),
+    collate_fn=train_dataset.collate_fn,
     batch_size=config.dataloader.batch_size,
     num_workers=config.dataloader.num_workers,
-    shuffle=True
+    shuffle=True,
 )
 
 
 valid_loader = DataLoader(
-    valid_dataset := Dataset('valid-dirty'),
+    valid_dataset := Dataset("valid-dirty"),
     collate_fn=valid_dataset.collate_fn,
     batch_size=config.dataloader.batch_size,
     num_workers=config.dataloader.num_workers,
-    shuffle=False
+    shuffle=False,
 )
 
 
@@ -44,7 +44,7 @@ if cuda.device_count() > 1:
 optimizer = optim.AdamW(
     model.parameters(),
     lr=config.optimizer.lr,
-    weight_decay=config.optimizer.weight_decay
+    weight_decay=config.optimizer.weight_decay,
 )
 
 ##############################################################################
@@ -52,64 +52,68 @@ optimizer = optim.AdamW(
 ##############################################################################
 
 task = wandb.init(
-    name=config.id, 
-    project='llm-sentinel', 
-    entity='deep-learner',
-    id='5gk3khsd',
-    resume='must',
+    name=config.id,
+    project="llm-sentinel",
+    entity="deep-learner",
+    id="5gk3khsd",
+    resume="must",
 )
 
-wandb.save('detector/models/arbitrary/__init__.py')
-wandb.save('detector/models/arbitrary/__main__.py')
-wandb.save('detector/models/arbitrary/dataset.py')
-wandb.save('detector/models/arbitrary/model.py')
-wandb.save('detector/models/arbitrary/settings.yaml')
-wandb.save('detector/models/arbitrary/types.py')
-wandb.save('detector/models/arbitrary/utilities.py')
+wandb.save("detector/t5_sentinel/__init__.py")
+wandb.save("detector/t5_sentinel/__main__.py")
+wandb.save("detector/t5_sentinel/dataset.py")
+wandb.save("detector/t5_sentinel/model.py")
+wandb.save("detector/t5_sentinel/settings.yaml")
+wandb.save("detector/t5_sentinel/types.py")
+wandb.save("detector/t5_sentinel/utilities.py")
 
-cache = f'storage/{config.id}'
+cache = f"storage/{config.id}"
 os.path.exists(cache) or os.makedirs(cache)
 
-if os.path.exists(f'{cache}/state.pt'):
-    state = torch.load(f'{cache}/state.pt')
-    model.load_state_dict(state['model'])
-    optimizer.load_state_dict(state['optimizer'])
-    startEpoch = state['epochIter'] + 1
-    bestValidationAccuracy = state['validAccuracy']
+if os.path.exists(f"{cache}/state.pt"):
+    state = torch.load(f"{cache}/state.pt")
+    model.load_state_dict(state["model"])
+    optimizer.load_state_dict(state["optimizer"])
+    startEpoch = state["epochIter"] + 1
+    bestValidationAccuracy = state["validAccuracy"]
 else:
     startEpoch = 0
-    bestValidationAccuracy = float('-inf')
+    bestValidationAccuracy = float("-inf")
 
 
 ##############################################################################
 # Training and Validation
 ##############################################################################
 for epoch in range(startEpoch, config.epochs):
-    tqdm.write('Epoch {}'.format(epoch + 1))
-    learnRate = optimizer.param_groups[0]['lr']
+    tqdm.write("Epoch {}".format(epoch + 1))
+    learnRate = optimizer.param_groups[0]["lr"]
     trainLoss, trainAccuracy = train(model, optimizer, train_loader)
     validAccuracy = validate(model, valid_loader)
 
-    wandb.log({
-        'Training Loss': trainLoss,
-        'Training Accuracy': trainAccuracy * 100,
-        'Validation Accuracy': validAccuracy * 100,
-        'Learning Rate': learnRate
-    })
+    wandb.log(
+        {
+            "Training Loss": trainLoss,
+            "Training Accuracy": trainAccuracy * 100,
+            "Validation Accuracy": validAccuracy * 100,
+            "Learning Rate": learnRate,
+        }
+    )
 
-    tqdm.write('Training Accuracy {:.2%}'.format(trainAccuracy))
-    tqdm.write('Training Loss {:.4f}'.format(trainLoss))
-    tqdm.write('Validation Accuracy {:.2%}'.format(validAccuracy))
-    tqdm.write('Learning Rate {:.4f}'.format(learnRate))
+    tqdm.write("Training Accuracy {:.2%}".format(trainAccuracy))
+    tqdm.write("Training Loss {:.4f}".format(trainLoss))
+    tqdm.write("Validation Accuracy {:.2%}".format(validAccuracy))
+    tqdm.write("Learning Rate {:.4f}".format(learnRate))
 
     checkpoint = {
         "epochIter": epoch,
-        "model": model.module.state_dict() if isinstance(model, nn.DataParallel) else model.state_dict(),
+        "model": model.module.state_dict()
+        if isinstance(model, nn.DataParallel)
+        else model.state_dict(),
         "optimizer": optimizer.state_dict(),
         "validAccuracy": validAccuracy,
     }
 
     if validAccuracy > bestValidationAccuracy:
         bestValidationAccuracy = validAccuracy
-        torch.save(checkpoint, f'{cache}/state.pt')
-        tqdm.write('Checkpoint Saved!')
+        torch.save(checkpoint, f"{cache}/state.pt")
+        tqdm.write("Checkpoint Saved!")
